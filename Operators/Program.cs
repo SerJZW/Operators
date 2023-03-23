90a5b7d713877750b796e788eb659017870c168b
ZemtsovSD <ZemtsovSD@302D-3>
2023-03-23 12:27:48 +0700

    Добавьте файлы проекта.

---
+++ Operators/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            int choice = 8;
+            RationalNumbers a = new RationalNumbers(3, 5);
+            RationalNumbers b = new RationalNumbers(4, 7);
+
+            while (true)
+            {
+                a.ToString();
+                b.ToString();
+                Console.WriteLine("Что вы хотите сделать?\n1)Сложение \n2)Вычитание \n3)Умножение \n4)Деление \n5)Первая больше второй \n6)Первая меньше второй \n7)Равны ли дроби \n8)Не равны ли дроби");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    
+                    
+                    case 1:
+                        //Сумма
+                        RationalNumbers addition = a + b;
+                        Console.WriteLine($"Результат сложения: {addition}\n");
+                        continue;
+                    case 2:
+                        //Вычитание
+                        RationalNumbers substraction = a - b;
+                        Console.WriteLine($"Результат вычитания: {substraction}\n");
+                        continue;
+                    case 3:
+                        //Умножение
+                        RationalNumbers multiplication = a * b;
+                        Console.WriteLine($"Результат умножения: {multiplication}\n");
+                        continue;
+                    case 4:
+                        //Деление
+                        RationalNumbers division = a / b;
+                        Console.WriteLine($"Результат деления: {division}\n");
+                        continue;
+                        //Все остальное методы сравнения дробей
+                    case 5:
+                        bool isGreater = a > b;
+                        Console.WriteLine($"Первая дробь больше второй? {isGreater}\n");
+                        continue;
+                    case 6:
+                        bool isLess = a < b;
+                        Console.WriteLine($"Первая дробь меньше второй? {isLess}\n");
+                        continue;
+                    case 7:
+                        bool isEquals = a == b;
+                        Console.WriteLine($"Обе дроби равны? {isEquals}\n");
+                        continue;
+                    case 8:
+                        bool isNotEquals = a != b;
+                        Console.WriteLine($"Обе дроби не равны? {isNotEquals}\n");
+                        continue;
+                    default:
+                        break;
+                }
+                break;
+            }
+
+        }
+    }
+}
+
---
+++ Operators/RationalNumbers.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Operators
+{
+    public class RationalNumbers
+    {
+        //Поля класса
+        public int numerator { get; }
+        public int denominator { get; }
+        // Конструктор
+        public RationalNumbers(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new ArgumentException("Знаменатель не может быть равен нулю.");
+            }
+
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+        // Операция сложения
+        public static RationalNumbers operator +(RationalNumbers a, RationalNumbers b)
+        {
+            int numerator = a.numerator * b.denominator + b.numerator * a.denominator;
+            int commonDenominator = a.denominator * b.denominator;
+            return new RationalNumbers(numerator, commonDenominator);
+        }
+        // Операция вычитания
+        public static RationalNumbers operator -(RationalNumbers a, RationalNumbers b)
+        {
+            int numerator = a.numerator * b.denominator - b.numerator * a.denominator;
+            int commonDenominator = a.denominator * b.denominator;
+            return new RationalNumbers(numerator, commonDenominator);
+        }
+        // Операция умножения
+        public static RationalNumbers operator *(RationalNumbers a, RationalNumbers b)
+        {
+            int numerator = a.numerator * b.numerator;
+            int denominator = a.denominator * b.denominator;
+
+            return new RationalNumbers(numerator, denominator);
+        }
+        // Операция деления
+        public static RationalNumbers operator /(RationalNumbers a, RationalNumbers b)
+        {
+            int numerator = a.numerator * b.denominator;
+            int denominator = a.denominator * b.numerator;
+            return new RationalNumbers(numerator, denominator);
+        }
+        // Операция сравнения что первая дробь больше второй
+        public static bool operator >(RationalNumbers a, RationalNumbers b)
+        {
+            return a.numerator * b.denominator > b.numerator * a.denominator;
+        }
+        // Операция сравнения что первая дробь меньше второй
+        public static bool operator <(RationalNumbers a, RationalNumbers b)
+        {
+            return a.numerator * b.denominator < b.numerator * a.denominator;
+        }
+        // Операция равности дробей
+        public static bool operator ==(RationalNumbers a, RationalNumbers b)
+        {
+            return a.numerator * b.denominator == b.numerator * a.denominator;
+        }
+        // Операция неравности дробей
+        public static bool operator !=(RationalNumbers a, RationalNumbers b)
+        {
+            return a.numerator * b.denominator != b.numerator * a.denominator;
+        }
+        // Вывод значения
+        public override string ToString()
+        {
+            Console.WriteLine($"{numerator}/{denominator}");
+            return $"{numerator}/{denominator}";
+            
+        }
+    }
+}
